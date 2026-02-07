27a69a8d69f57efeac6d3c1ee4b89a8f050b054f
Akarsh Dhingra <dhingraaakarsh43@gmail.com>
2026-02-07 21:51:10 +0530

    Integrate Prisma Client and update database schema to include Group and Membership models, enhancing expense tracking capabilities.

--- expense-tracker-backend/index.js
+++ expense-tracker-backend/index.js
@@ -1,13 +1,15 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
+import { PrismaClient } from "@prisma/client";
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const prisma = new PrismaClient();
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server is running on port ${process.env.PORT}`);
---
+++ expense-tracker-backend/prisma/migrations/20260207161708_init/migration.sql
@@ -0,0 +1,41 @@
+/*
+  Warnings:
+
+  - Added the required column `groupId` to the `Expense` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Expense" DROP CONSTRAINT "Expense_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "Expense" ADD COLUMN     "groupId" TEXT NOT NULL,
+ALTER COLUMN "userId" DROP NOT NULL;
+
+-- CreateTable
+CREATE TABLE "Group" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Group_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Membership" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "groupId" TEXT NOT NULL,
+
+    CONSTRAINT "Membership_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Expense" ADD CONSTRAINT "Expense_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Expense" ADD CONSTRAINT "Expense_groupId_fkey" FOREIGN KEY ("groupId") REFERENCES "Group"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Membership" ADD CONSTRAINT "Membership_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Membership" ADD CONSTRAINT "Membership_groupId_fkey" FOREIGN KEY ("groupId") REFERENCES "Group"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
