9d0a83210c126985277523f3cd893b7bfcbba4f2
Akarsh Dhingra <dhingraaakarsh43@gmail.com>
2026-02-07 16:21:33 +0530

    Add exploratory comments in TamboWrapper for project direction

--- app/TamboWrapper.tsx
+++ app/TamboWrapper.tsx
@@ -15,3 +15,7 @@ export function TamboWrapper({ children }: { children: React.ReactNode }) {
     </TamboProvider>
   );
 }
+
+// We were on charlies anc exploring it next 
+// We are going to move towards building the idea of the projet and what we can potentially build with it.
+// We are going to build a prototype of the project and then we will see if we can build it.
